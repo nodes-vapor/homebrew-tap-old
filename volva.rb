835e2f68cd8341ae29321a34b1fd6ac4b698b2ca
Brett R. Toomey <brto@nodes.dk>
2018-08-31 13:35:33 +0200

    Remove resource

--- volva.rb
+++ volva.rb
@@ -5,17 +5,9 @@ class Volva < Formula
     url "https://github.com/BrettRToomey/volva/archive/0.0.1.tar.gz"
     sha256 "a6a3e5b9979be9add966435ac9d71e64c65bf811e2c20c37ad2e3a26bf2ba009"
 
-
-    resource "volva" do
-        url "https://github.com/BrettRToomey/volva/archive/0.0.1.tar.gz"
-        sha256 "a6a3e5b9979be9add966435ac9d71e64c65bf811e2c20c37ad2e3a26bf2ba009"
-    end
-
     def install
-        resource("volva").stage {
-            system "make"
-            system "make", "install", "PREFIX=#{prefix}", "INSTALL_BINARY=no"
-            bin.install "volv"
-        }
+        system "make"
+        system "make", "install", "PREFIX=#{prefix}", "INSTALL_BINARY=no"
+        bin.install "volv"
     end
 end
