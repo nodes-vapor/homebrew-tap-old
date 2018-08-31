8ee36b30f6bd79eff5c1cf939350d2714b22fb81
Brett R. Toomey <brto@nodes.dk>
2018-08-31 12:38:55 +0200

    Volva

---
+++ volva.rb
@@ -0,0 +1,10 @@
+class Volva > Formula
+    desc "Command line tool for common Vapor tasks"
+    version "0.0.1"
+    url "https://github.com/BrettRToomey/volva/archive/0.0.1.tar.gz"
+    sha256 "69245fd67fbb1a0183ec509f8c324230d20c35fe2a1cc016f3cac5faac5bc8a5"
+
+    def install
+        system "make", "install"
+    end
+end
