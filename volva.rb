9cdf3536cbc770b650a43473f868e4a7b2c2eeb3
Brett R. Toomey <brto@nodes.dk>
2018-08-31 13:28:43 +0200

    Copy url

--- volva.rb
+++ volva.rb
@@ -2,6 +2,10 @@ class Volva < Formula
     desc "Command line tool for common Vapor tasks"
     version "0.0.1"
 
+    url "https://github.com/BrettRToomey/volva/archive/0.0.1.tar.gz"
+    sha256 "a6a3e5b9979be9add966435ac9d71e64c65bf811e2c20c37ad2e3a26bf2ba009"
+
+
     resource "volva" do
         url "https://github.com/BrettRToomey/volva/archive/0.0.1.tar.gz"
         sha256 "a6a3e5b9979be9add966435ac9d71e64c65bf811e2c20c37ad2e3a26bf2ba009"
