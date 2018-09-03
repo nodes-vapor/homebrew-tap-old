ea1c33f16b3e234f35efc5347b4d5fc5af59b583
Brett R. Toomey <brto@nodes.dk>
2018-09-03 14:42:52 +0200

    Updated volva

--- volva.rb
+++ volva.rb
@@ -1,6 +1,6 @@
 class Volva < Formula
     desc "Command line tool for common Vapor tasks"
-    version "0.0.4"
+    version "0.0.5"
 
     url "https://github.com/BrettRToomey/volva/archive/#{version}.tar.gz"
     sha256 "c9fce0e2293911e4dc86723ca3d42a282aaa9fd4bb84f40b405f2fe9f13d3290"
