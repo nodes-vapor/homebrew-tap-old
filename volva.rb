1c60e64d43396ecb0fa74c9eafbbf38d14f50bb0
Brett R. Toomey <brto@nodes.dk>
2018-09-03 13:05:23 +0200

    Updated volva version

--- volva.rb
+++ volva.rb
@@ -1,9 +1,9 @@
 class Volva < Formula
     desc "Command line tool for common Vapor tasks"
-    version "0.0.1"
+    version "0.0.2"
 
     url "https://github.com/BrettRToomey/volva/archive/0.0.1.tar.gz"
-    sha256 "a6a3e5b9979be9add966435ac9d71e64c65bf811e2c20c37ad2e3a26bf2ba009"
+    sha256 "1a24344514aeae9ea8f414f314ceb0d69f46618e5650e8fa8555215f36da04cf"
 
     def install
         system "make"
